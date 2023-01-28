f1c43919d01de0a95eba3cdf3a4cc77a5874e4e2
Charly Aguettaz <caguettaztaz@gmail.com>
2023-01-28 18:20:59 +0100

    position

---
+++ src/rover/main.java
@@ -0,0 +1,16 @@
+package rover;
+
+
+class Main {
+  public static void main(String[] args) {
+
+    Position position = new Position(0, 0, "North");
+
+    System.out.println(position.cardinal);
+    position.turnLeft();
+    System.out.println(position.cardinal);
+    position.turnRight();
+    System.out.println(position.cardinal);
+
+  }
+}
